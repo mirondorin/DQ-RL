0eec420fe82edeeb035a79e5671f9f0245275a3f
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-03-04 12:35:42 +0200

    Added braindead mob AI

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
 export var SPEED = 90
-export var JUMPSPEED = 80
+export var JUMPSPEED = 320
 onready var GRAVITY = get_node('../GlobalSettings').GRAVITY
 onready var player = $'../PlayableCharacter'
 onready var attack_timer = $'AttackCooldown'
+onready var jump_timer = $'JumpCooldown'
+
 
 export var health = 20
 var is_dead = false
@@ -12,16 +14,37 @@ var is_dead = false
 var velocity = Vector2()
 export var follow = true
 var direction = 1
-
+var in_jump = false
+var can_jump = false
+var can_attack = true
+var jump_intensity = 0
+var start_time = -100
 var attack_damage = 10
 var attack_cooldown = 1.5	
-	
+var jump_cooldown = 5
+
 func _ready():
 	attack_timer.wait_time = attack_cooldown	
+	jump_timer.wait_time = jump_cooldown
+	jump_timer.start()
 	
+func jump(time):
+	var speed = -JUMPSPEED/20
+	if is_on_floor():
+		in_jump = true
+		jump_intensity = 20
+		start_time = OS.get_ticks_msec()
+	var current_time = OS.get_ticks_msec()
+	if current_time-start_time>50:
+		jump_intensity = 0
+	else:
+		start_time=current_time
+		
+	jump_intensity*=0.80
+	speed*=jump_intensity
+	return speed
 
 func follow_player():
-	
 	if position.x < player.position.x:
 		direction = 1
 	else:
@@ -33,7 +56,7 @@ func follow_player():
 func attack_player(collider):
 	collider.call("take_damage", attack_damage)
 	move_and_slide(Vector2(velocity.x + 2000*direction*-1, velocity.y), Vector2(0, -1))
-	follow = false
+	can_attack = false
 	attack_timer.start()
 
 func solve_animation(velocity,delta):
@@ -55,20 +78,24 @@ func _physics_process(delta):
 	follow_player()
 	velocity.y += delta * GRAVITY
 	velocity.x = SPEED * direction
-	solve_animation(velocity, delta)
-	
 	if is_on_floor():
-		velocity.y=0
-		var jump_intensity = 0
-		var in_jump=false
-	
+		velocity.y = 0
+		jump_intensity = 0
+		in_jump=false
 	if is_on_ceiling():
 		velocity.y=max(0,velocity.y)
+		
+	if can_jump and follow:
+		velocity.y += jump(delta)
+		can_jump = false
+	
+	solve_animation(velocity, delta)
 	
 	#solve_animation(velocity,delta)
 	#move_and_collide(velocity)
 	
 	move_and_slide(velocity, Vector2(0, -1))
+	
 		
 	var previous = get_slide_count()
 	for i in get_slide_count():
@@ -86,13 +113,14 @@ func on_take_damage():
 			is_dead = true
 			queue_free()
 			get_node("../EnemySpawner").spawn()
-
 	follow = false
+	
 	attack_timer.start()
 
 func take_damage(value):
-	health -= value
-	on_take_damage()
+	if follow:
+		health -= value
+		on_take_damage()
 
 func _on_DetectArea_body_entered(body):
 	if body == player:
@@ -103,4 +131,9 @@ func _on_DetectArea_body_exited(body):
 		follow = false
 
 func _on_AttackCooldown_timeout():
+	can_attack = true
 	follow = true
+	
+
+func _on_JumpCooldown_timeout():
+	can_jump = true
