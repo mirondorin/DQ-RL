f0c1aad0f17d00bb84a3e6b18b09f578fe38c90a
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-03-13 18:56:20 +0200

    Syncronized weapons

--- scripts/Weapons/Weapon.gd
+++ scripts/Weapons/Weapon.gd
@@ -8,10 +8,12 @@ func _ready():
 	position = Vector2(50, 40)
 
 func attack():
-	play_animation(attack_anim_names['attack'])
+	if is_network_master():
+		rpc_unreliable("play_animation", attack_anim_names['attack'])
 
 func special_attack():
-	play_animation(attack_anim_names['special-attack'])
+	if is_network_master():
+		rpc_unreliable("play_animation", attack_anim_names['special-attack'])
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("hitbox"):
