33b43421f4fdbc2383170a600f54cb4286490f80
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-04-18 21:56:25 +0300

    Added extrapolation

--- scripts/MainScene.gd
+++ scripts/MainScene.gd
@@ -58,6 +58,7 @@ func SpawnNewPlayer(player_id, spawn_position):
 
 
 func DespawnPlayer(player_id):
+	yield(get_tree().create_timer(0.2), "timeout")
 	print("Despawning player with id = " + str(player_id))
 	if get_node("YSort/OtherPlayers/" + str(player_id)) != null:
 		get_node("YSort/OtherPlayers/" + str(player_id)).queue_free()
@@ -96,19 +97,34 @@ func update_world_state(s_world_state):
 func _physics_process(delta):
 	var render_time = OS.get_system_time_msecs() - interpolation_offset
 	if world_state_buffer.size() > 1:
-		while world_state_buffer.size() > 2 and render_time > world_state_buffer[1].T:
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[2].T:
 			world_state_buffer.remove(0)
-		var interpolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"])
-		for player in world_state_buffer[1].keys():
-			if str(player) == "T":
-				continue
-			if player == get_tree().get_network_unique_id():
-				continue
-			if not world_state_buffer[0].has(player):
-				continue
-			if get_node("YSort/OtherPlayers").has_node(str(player)):
-				var new_position = lerp(world_state_buffer[0][player]["P"], world_state_buffer[1][player]["P"], interpolation_factor)
-				get_node("YSort/OtherPlayers/" + str(player)).MovePlayer(new_position)
-			else:
-				print("spawning player")
-				SpawnNewPlayer(player, world_state_buffer[1][player]["P"])
+		if world_state_buffer.size() > 2:
+			var interpolation_factor = float(render_time - world_state_buffer[1]["T"]) / float(world_state_buffer[2]["T"] - world_state_buffer[1]["T"])
+			for player in world_state_buffer[2].keys():
+				if str(player) == "T":
+					continue
+				if player == get_tree().get_network_unique_id():
+					continue
+				if not world_state_buffer[1].has(player):
+					continue
+				if get_node("YSort/OtherPlayers").has_node(str(player)):
+					var new_position = lerp(world_state_buffer[1][player]["P"], world_state_buffer[2][player]["P"], interpolation_factor)
+					get_node("YSort/OtherPlayers/" + str(player)).MovePlayer(new_position)
+				else:
+					print("spawning player")
+					SpawnNewPlayer(player, world_state_buffer[2][player]["P"])
+		elif render_time > world_state_buffer[1].T:
+			var extrapolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"]) - 1.00
+			for player in world_state_buffer[1].keys():
+				if str(player) == "T":
+					continue
+				if player == get_tree().get_network_unique_id():
+					continue
+				if not world_state_buffer[1].has(player):
+					continue
+				if get_node("YSort/OtherPlayers").has_node(str(player)):
+					var position_delta = (world_state_buffer[1][player]["P"] - world_state_buffer[0][player]["P"])
+					var new_position = world_state_buffer[1][player]["P"] + (position_delta * extrapolation_factor)
+					get_node("YSort/OtherPlayers/" + str(player)).MovePlayer(new_position)
+					
