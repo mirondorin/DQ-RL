ec0778b20abdc77a248fd79c921de6d0ff9198fd
Dorin <mirondorin99@gmail.com>
2021-05-26 13:29:53 +0300

    Added 2nd boss attack

--- scripts/mobs/BoDBoss.gd
+++ scripts/mobs/BoDBoss.gd
@@ -1,8 +1,8 @@
 extends "res://scripts/mobs/Mob_base.gd"
 
-var bullet
+var can_slash_attack = true
+var slash_attacking = false
 var can_cast_attack = true
-var can_jump_attack = true
 var cast_attacking = false
 
 	
@@ -23,23 +23,59 @@ func _process(delta):
 	pass
 
 
+func flip_sprite():
+	if x_direction == -1:
+		$AnimatedSprite.offset = Vector2(70, 0)
+		$Hurtbox.scale.x *= -1.0
+		$AttackArea.scale.x *= -1.0
+	else:
+		$AnimatedSprite.offset = Vector2(0, 0)
+		$Hurtbox.scale.x = abs($Hurtbox.scale.x)
+		$AttackArea.scale.x = abs($AttackArea.scale.x)
+
 func solve_animation(velocity):
-	if not key_has_value(animation_dict, "flip_h", (x_direction < 0)):
-		if x_direction == -1:
-			$AnimatedSprite.offset = Vector2(70, 0)
+	if x_direction != 0:
+		animation_change = true
+		if not key_has_value(animation_dict, "flip_h", (x_direction < 0)):
+			flip_sprite()
+			animation_dict["flip_h"] = (x_direction < 0)
+			new_animation_dict["flip_h"] = (x_direction < 0)
+			animation_change = true
+		if not key_has_value(animation_dict, "animation", "walk"):
+			animation_dict["animation"] = 'walk'
+			new_animation_dict["animation"] = 'walk'
+			animation_change = true
+	if velocity.x == 0:	
+		if slash_attacking:
+			if not key_has_value(animation_dict, "animation", "attack"):
+				animation_dict["animation"] = 'attack'
+				new_animation_dict["animation"] = 'attack'
+				animation_change = true
 		else:
-			$AnimatedSprite.offset = Vector2(0, 0)
-		animation_dict["flip_h"] = (x_direction < 0)
-		new_animation_dict["flip_h"] = (x_direction < 0)
-		animation_change = true
-	if not key_has_value(animation_dict, "animation", "walk"):
-		animation_dict["animation"] = 'walk'
-		new_animation_dict["animation"] = 'walk'
-		animation_change = true
+			if not key_has_value(animation_dict, "animation", "idle"):
+				animation_dict["animation"] = "idle" 
+				new_animation_dict["animation"] = "idle" 
+				animation_change = true
+
 
 
 func attack_player(player):
 	player.take_damage(attack_damage, 0, Vector2(x_direction, 0), 10)  # TODO: ensure that player takes damage only once, and takes it everywhere
+
+
+func slash_attack():
+	if can_slash_attack:
+		follow = false
+		slash_attacking = true
+		$AnimationPlayer.play("slash_attack")
+		$Hurtbox/CollisionShape2D.set_deferred("disabled", false)
+		can_slash_attack = false
+		$SlashAttackCooldown.start()
+		
+func slash_attack_reset():
+	follow = true
+	slash_attacking = false
+	$Hurtbox/CollisionShape2D.set_deferred("disabled", true)
 
 
 func _on_Hurtbox_area_entered(area):
@@ -48,6 +84,7 @@ func _on_Hurtbox_area_entered(area):
 		if owner.is_in_group('players'):
 			attack_player(owner)
 
+
 func _on_AnimatedSprite_frame_changed():
 	pass
 
@@ -55,5 +92,12 @@ func _on_AnimatedSprite_frame_changed():
 func _on_CastAttackCooldown_timeout():
 	can_cast_attack = true
 
-func _on_AttackCooldown_timeout():
-	can_jump_attack = true
+
+func _on_SlashAttackCooldown_timeout():
+	can_slash_attack = true
+
+
+func _on_AttackArea_body_entered(body):
+	if body in in_area:
+		if body.is_in_group("players"):
+			slash_attack()
