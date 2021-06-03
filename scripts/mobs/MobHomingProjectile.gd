23f76c11fc8737747d2464933914b0583ba64bc0
Paraschiv Tudor <gparaschiv@gmail.com>
2021-06-03 08:36:36 +0300

    Update MobHomingProjectile.gd

--- scripts/mobs/MobHomingProjectile.gd
+++ scripts/mobs/MobHomingProjectile.gd
@@ -12,7 +12,8 @@ func _ready():
 sync func do_attack():
 	var bullet_inst = bullet.instance()
 	bullet_inst.group_to_detect = 'players'
-	bullet_inst.to_follow = in_area[0]
+	if range(in_area.size()).has(0):
+		bullet_inst.to_follow = in_area[0]
 	bullet_inst.direction = -1 if int($AnimatedSprite.flip_h) else 1
 	get_tree().get_root().add_child(bullet_inst)
 	bullet_inst.global_position = self.global_position
